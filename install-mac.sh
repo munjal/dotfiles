6af884d6e834396768088ce90a98e4f29aaad6f9
Munjal Budhabhatti <munjalpb2000@gmail.com>
2019-02-24 19:07:47 -0300

    Fixed issues with if - fi

--- install-mac.sh
+++ install-mac.sh
@@ -7,14 +7,17 @@ fancy_echo() {
 }
 
 fancy_echo "Installing ohh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]
+then
+   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
+fancy_echo "Installing Homebrew ..."
 if ! command -v brew >/dev/null; then
-  fancy_echo "Installing Homebrew ..."
     curl -fsS \
       'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
 
-    config checkout ~/.zshrc
+    config checkout $HOME/.zshrc
 
     append_to_zshrc '# recommended by brew doctor'
 
@@ -24,7 +27,7 @@ if ! command -v brew >/dev/null; then
     export PATH="/usr/local/bin:$PATH"
 fi
 
-if [-f "~/.ssh/id_rsa"]
+if [ ! -f "$HOME/.ssh/id_rsa" ]
 then
   fancy_echo "Installing git keys"
   fancy_echo "What name would you want in your Git commit messages?"
@@ -32,25 +35,25 @@ then
   fancy_echo "What's your email address associated with Github?"
   read github_email
 
-  fancy_echo "Writing to ~/.gitconfig"
+  fancy_echo "Writing to $HOME/.gitconfig"
   echo "
   [user]
           name = $github_name
           email = $github_email
-     " >> ~/.gitconfig
+     " >> $HOME/.gitconfig
 
 
   fancy_echo "Generating & configuringssh keys"
-  ssh-keygen -t rsa -b 4096 -C $github_email -N "" -f ~/.ssh/id_rsa
+  ssh-keygen -t rsa -b 4096 -C $github_email -N "" -f $HOME/.ssh/id_rsa
   eval "$(ssh-agent -s)"
 
-  touch ~/.ssh/config
+  touch $HOME/.ssh/config
   echo "Host *
      AddKeysToAgent yes
      UseKeychain yes
-     IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
+     IdentityFile $HOME/.ssh/id_rsa" >> $HOME/.ssh/config
 
-  ssh-add -K ~/.ssh/id_rsa
+  ssh-add -K $HOME/.ssh/id_rsa
 
   fancy_echo "Please copy your ssh public keys to github"
   open https://help.github.com/en/articles/adding-a-new-ssh-key-to-your-github-account
@@ -60,12 +63,18 @@ fancy_echo "Installing brew bundles..."
 brew bundle
 
 fancy_echo "Installing spacemacs"
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+if [ ! -d "$HOME/.emacs.d" ]
+then
+    git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+fi
 
 fancy_echo "Installing asdf"
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-cd ~/.asdf
-git checkout "$(git describe --abbrev=0 --tags)"
+if [ ! -d "$HOME/.asdf" ]
+then
+    git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf
+    cd $HOME/.asdf
+    git checkout "$(git describe --abbrev=0 --tags)"
+fi
 
 . $HOME/.asdf/asdf.sh
 . $HOME/.asdf/completions/asdf.bash
@@ -93,12 +102,18 @@ install_asdf_plugin() {
 fancy_echo "Installing asdf plugins"
 export KERL_CONFIGURE_OPTIONS="--disable-debug --without-javac"
 install_asdf_plugin erlang
-
 install_asdf_plugin elixir
 
-if [ ! -d "/Application Visual Studio/Code.app"]
+fancy_echo "Installing Visual Studio Code"
+if [ ! -d "/Applications/Visual Studio Code.app" ]
 then
     curl -Lo /Applications/Visual\ Studio\ Code.zip https://update.code.visualstudio.com/1.31.1/darwin/stable
     tar -xf /Applications/Visual\ Studio\ Code.zip
 fi
 
+fancy_echo "Installing Docker"
+if [ ! -d "/Applications/Docker.app" ]
+then
+    curl -Lo Downloads/Docker.dmg  https://download.docker.com/mac/stable/Docker.dmg
+    open Downloads/Docker.dmg
+fi

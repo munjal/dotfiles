e6e412d095b2cb5635006f441c876f970fdddf4c
Leandro Heck <leoheck@gmail.com>
2019-02-26 12:56:29 -0300

    Normalize identation

--- install-mac.sh
+++ install-mac.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
 fancy_echo() {
-  local fmt="$1"; shift
-
-  printf "\n$fmt\n" "$@"
+    local fmt="$1"; shift
+    printf "\n$fmt\n" "$@"
 }
 
 function config {
-    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@  
+    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 
 fancy_echo "Installing ohh-my-zsh"
 if [ ! -d "$HOME/.oh-my-zsh" ]
 then
-   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 fancy_echo "Installing Homebrew ..."
 if ! command -v brew >/dev/null; then
     curl -fsS \
-      'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
+        'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
 
     change this... not working
     config checkout $HOME/.zshrc
@@ -34,34 +33,33 @@ fi
 
 if [ ! -f "$HOME/.ssh/id_rsa" ]
 then
-  fancy_echo "Installing git keys"
-  fancy_echo "What name would you want in your Git commit messages?"
-  read github_name
-  fancy_echo "What's your email address associated with Github?"
-  read github_email
+    fancy_echo "Installing git keys"
+    fancy_echo "What name would you want in your Git commit messages?"
+    read github_name
+    fancy_echo "What's your email address associated with Github?"
+    read github_email
 
-  fancy_echo "Writing to $HOME/.gitconfig"
-  echo "
-  [user]
-          name = $github_name
-          email = $github_email
-     " >> $HOME/.gitconfig
+    fancy_echo "Writing to $HOME/.gitconfig"
+    echo "
+    [user]
+        name = $github_name
+        email = $github_email
+    " >> $HOME/.gitconfig
 
+    fancy_echo "Generating & configuringssh keys"
+    ssh-keygen -t rsa -b 4096 -C $github_email -N "" -f $HOME/.ssh/id_rsa
+    eval "$(ssh-agent -s)"
 
-  fancy_echo "Generating & configuringssh keys"
-  ssh-keygen -t rsa -b 4096 -C $github_email -N "" -f $HOME/.ssh/id_rsa
-  eval "$(ssh-agent -s)"
+    touch $HOME/.ssh/config
+    echo "Host *
+        AddKeysToAgent yes
+        UseKeychain yes
+        IdentityFile $HOME/.ssh/id_rsa" >> $HOME/.ssh/config
 
-  touch $HOME/.ssh/config
-  echo "Host *
-     AddKeysToAgent yes
-     UseKeychain yes
-     IdentityFile $HOME/.ssh/id_rsa" >> $HOME/.ssh/config
+    ssh-add -K $HOME/.ssh/id_rsa
 
-  ssh-add -K $HOME/.ssh/id_rsa
-
-  fancy_echo "Please copy your ssh public keys to github"
-  open https://help.github.com/en/articles/adding-a-new-ssh-key-to-your-github-account
+    fancy_echo "Please copy your ssh public keys to github"
+    open https://help.github.com/en/articles/adding-a-new-ssh-key-to-your-github-account
 fi
 
 fancy_echo "Installing brew bundles..."
@@ -87,6 +85,7 @@ fi
 find_latest_asdf() {
   asdf list-all "$1" | grep -v - | tail -1 | sed -e 's/^ *//'
 }
+
 asdf_plugin_present() {
   $(asdf plugin-list | grep "$1" > /dev/null)
   return $?
